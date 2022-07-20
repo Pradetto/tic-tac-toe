510b4e1237dd034303af3d08e6c780a62664cdb2
Michael Pradetto <pradetto5@gmail.com>
2022-07-19 18:36:30 -0700

    completed JS functionality

--- scripts.js
+++ scripts.js
@@ -13,6 +13,9 @@ const gameBoard = (() =>{
 
     let boardContainer = document.querySelector('.boardContainer');
 
+    let playerOneSelections = [];
+    let playerTwoSelections = [];
+
     // creating board
     board.forEach((item) => {
         const square = document.createElement('div');
@@ -25,16 +28,61 @@ const gameBoard = (() =>{
 
     // adding event listener on each child
     let boardFunction = function (child,index){
-        child.classList.add('hello')
-        child.textContent = 'whom'
+        child.classList.add('selected')
+        child.textContent = game.activePlayer.marker
         child.style.pointerEvents = 'none'
-        console.log('this is the index ' + index)
+
+
+        game.remainingSpots -= 1
+
+
+        if (game.activePlayer === game.playerOne){
+            playerOneSelections.push(index)
+            game.checkWinner(playerOneSelections)
+        } else {
+            playerTwoSelections.push(index)
+            game.checkWinner(playerTwoSelections)
+        }
+        if (game.winnerDeclared === false && game.remainingSpots > 0){
+            game.alertNextPlayer();
+            game.nextPlayer();
+        } else if (game.winnerDeclared === true) {
+            return
+        }
+
+        if (game.remainingSpots === 0){
+            game.declareTie()
+            return
+        };
+
+
     };
 
     boardChildren.forEach((child,index) => {
         child.addEventListener('click', ()=> boardFunction(child,index));
     });
 
+    const restartButton = document.querySelector('.resetGame');
+
+    restartButton.addEventListener('click', ()=> {
+        location.reload();
+        return false;
+        // gameBoard.board = [];
+        // for (let i = 0; i < 9;i++){
+        //     gameBoard.board.push(i);
+        //     console.log(gameBoard.board)
+    // };
+    });
+
+    let deactivateBoard = () =>{
+        boardChildren.forEach((child) => {
+            child.style.pointerEvents = 'none'
+        })
+    }
+
+    return {
+        deactivateBoard
+    }
 
 })();
 
@@ -46,11 +94,12 @@ const game = (() => {
 
     // starting point
     let activePlayer = playerOne
-    let winnerDeclared = fasle;
+    let winnerDeclared = false;
     let remainingSpots = 9;
     
     // selectors
-
+    let subtext = document.querySelector('.subtext'); // display winner/tie
+    let playerName = document.querySelector('.player-name'); // purpose: alert player turn
 
     // winning conditions
     const winningAxes = [
@@ -65,15 +114,60 @@ const game = (() => {
     ];
 
     // check winner
+    function checkWinner(selections){
+        winningAxes.forEach((item,index) => {
+            if(item.every(element => selections.includes(element))){
+                game.winnerDeclared = true
+                console.log('winner found')
+                subtext.innerHTML = `<b>${this.activePlayer.name} wins!</b>`;
+                console.log('True')
+                gameBoard.deactivateBoard();
+            }
+        })
+    }
+
+
+        // winningAxes.forEach((item, index) => { // [0, 1, 2, 3, 4, 5, 6, 7]
+        //     if (gameBoard.board[item[0]] === this.activePlayer.marker && gameBoard.board[item[1]] === this.activePlayer.marker && gameBoard.board[item[2]] === this.activePlayer.marker) {
+        //         console.log('winner!');
+        //         subtext.innerHTML = `<b>${this.activePlayer.name} wins!</b>`;
+        //         this.winnerDeclared = true;
+        //     } 
+        // });
+
+
 
     // alert next player
+    function alertNextPlayer(){
+        this.activePlayer === playerOne ? playerName.textContent = 'Player Two' : playerName.textContent = 'Player One';
+    }
 
     // next player
-
+    function nextPlayer(){
+        this.activePlayer === playerOne ? game.activePlayer = playerTwo : game.activePlayer = playerOne;
+    }
     // declare tie
+    function declareTie(){
+        subtext.innerHTML = "<b>Tie game!</b>";
+    }
 
     //return
 
-
+    return {
+        activePlayer,
+        remainingSpots,
+        playerOne,
+        playerTwo,
+        checkWinner,
+        winnerDeclared,
+        declareTie,
+        alertNextPlayer,
+        nextPlayer,
+        declareTie
+    }
 
 })();
+
+function reset () {
+    gameBoard()
+}
